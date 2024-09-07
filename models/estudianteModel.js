eb6bd297a99547e9c42d47e83ccc974f0537107d
Brayan Diaz C <brayandiazc@gmail.com>
2024-09-07 02:27:29 -0500

    feat: Modelo estudiante creado

---
+++ models/estudianteModel.js
@@ -0,0 +1,7 @@
+// Simulamos una base de datos con listas de estudiantes y cursos
+const estudiantes = [
+  { id: 1, nombre: "Juan Pérez", edad: 20 },
+  { id: 2, nombre: "Ana Gómez", edad: 22 },
+];
+
+module.exports = estudiantes;
