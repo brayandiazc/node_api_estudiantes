b4d447fe637d71a99a0bf1d5ff842cc4702bae6d
Brayan Diaz C <brayandiazc@gmail.com>
2024-09-07 01:19:58 -0500

    feat: Configuración inicial del servidor

---
+++ index.js
@@ -0,0 +1,18 @@
+// Importamos el módulo de Express
+const express = require("express");
+
+// Creamos una instancia de la aplicación
+const app = express();
+
+// Definimos el puerto en el que correrá el servidor
+const PORT = process.env.PORT || 3000;
+
+// Creamos una ruta de ejemplo
+app.get("/", (req, res) => {
+  res.send("¡Hola, mundo! El servidor está corriendo.");
+});
+
+// Iniciamos el servidor
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
