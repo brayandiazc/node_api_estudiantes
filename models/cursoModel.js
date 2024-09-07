56fb919c53dfb0dc2cbc2667875cb4ed24faf176
Brayan Diaz C <brayandiazc@gmail.com>
2024-09-07 02:29:28 -0500

    feat: Modelo cursos creado

---
+++ models/cursoModel.js
@@ -0,0 +1,7 @@
+// Simulamos una base de datos con listas de cursos
+const cursos = [
+  { id: 1, nombre: "Curso de Node.js", duracion: "4 semanas" },
+  { id: 2, nombre: "Curso de React", duracion: "6 semanas" },
+];
+
+module.exports = cursos;
--- models/estudianteModel.js
+++ models/estudianteModel.js
@@ -1,4 +1,4 @@
-// Simulamos una base de datos con listas de estudiantes y cursos
+// Simulamos una base de datos con listas de estudiantes
 const estudiantes = [
   { id: 1, nombre: "Juan Pérez", edad: 20 },
   { id: 2, nombre: "Ana Gómez", edad: 22 },
